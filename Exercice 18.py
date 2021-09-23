f0c16510edf3f527d24f6d8501cac07c3a614864
NathanBergeron <90332849+NathanBergeron@users.noreply.github.com>
2021-09-23 15:13:49 +0200

    Add files via upload

---
+++ Exercice 18.py
@@ -0,0 +1,32 @@
+from turtle import *
+up()
+goto(-300, 0)
+down()
+left(90)
+forward(150)
+right(140)
+forward(200)
+left(140)
+forward(150)
+right(90)
+up()
+forward(75)
+down()
+right(90)
+forward(150)
+right(180)
+forward(75)
+right(90)
+forward(75)
+right(90)
+forward(75)
+right(180)
+forward(150)
+left(90)
+forward(75)
+
+
+
+
+
+
